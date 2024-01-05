d4bfd3de3708225c77a5a2c74e4041a799117ac9
Georgi <gurumov93@gmail.com>
2024-01-05 22:54:30 +0000

    hourly manual entry base functionality complete

--- src/components/salesAnalysisPage/salesAnalysisPageTemplate.js
+++ src/components/salesAnalysisPage/salesAnalysisPageTemplate.js
@@ -1,6 +1,7 @@
 import { html } from 'lit-html';
-import styles from './salesAnalysisPage.module.css'
-
+import styles from './salesAnalysisPage.module.css';
+import StringUtil from '../../utils/stringUtil.js';
+const stringUtil = new StringUtil();
 
 export const salesAnalysisPageTemplate = (slideOpen, hourlySales, hourlySalesInputHandler, hourlySalesChangeHandler, submitHandler) => html`
 <div class=${styles['page__container']}>
@@ -16,32 +17,34 @@ export const salesAnalysisPageTemplate = (slideOpen, hourlySales, hourlySalesInp
     <div @click=${slideOpen} data-id="hourly-sales-dropdown" class=${`${styles['section__bar']} ${styles['section__bar-closed']}`}>Hourly Sales
     </div>
     <form @submit=${submitHandler} id="hourly-sales-dropdown" class=${styles['expand__container']}>
-        <div @click=${slideOpen} data-id="hourly-sales-manual" class=${`${styles['section__bar']} ${styles['section__bar-inner']} ${styles['section__bar-closed']}`}>Manual Entry
+        <div @click=${slideOpen} data-id="hourly-sales-manual" class=${`${styles['section__bar']} ${styles['section__bar-inner']} ${styles['section__bar-closed']}`}>
+        <p>Hourly Manual Entry</p>
         </div>
         <div @change=${hourlySalesChangeHandler} @input=${hourlySalesInputHandler} id="hourly-sales-manual" class=${styles['expand__container']}>
             <div class=${styles['manual-entry-container']}>
                 ${Object.keys(hourlySales).map(weekday =>
                      html`
                         <div class=${styles['sales-summary-weekday']}>
-                            <h2>${weekday}</h2>
+                            <h2>${stringUtil.toPascalCase(weekday)}</h2>
                             <div class=${styles['manual-entry-weekday']}>
                                 ${Object.keys(hourlySales[weekday].hours).map(hour => html`
                                     <div class=${styles['hour-container']}>
                                         <label for="${weekday}-${hour}">${hour}:00</label>
-                                        <input maxLength="8" @click=${(e) => e.currentTarget.select()} for="${weekday}-${hour}" name="${weekday}-${hour}" value=${hourlySales[weekday].hours[hour]}>£</input>
+                                        <p>£</p>
+                                        <input maxLength="8" @click=${(e) => e.currentTarget.select()} for="${weekday}-${hour}" name="${weekday}-${hour}" value=${hourlySales[weekday].hours[hour]}></input>
                                     </div>
                                 `)}
                             </div>
                             <div class=${styles['hourly-totals-container']}>
                                 <div class=${styles['hourly-totals-field']}>
                                     <label for="total-value">Total</label>
-                                    <input class=${styles['manual-totals']} disabled id="total-value-${weekday}" value=${hourlySales[weekday].totals.total}></input>
                                     <p>£</p>
+                                    <input class=${styles['manual-totals']} disabled id="total-value-${weekday}" value=${hourlySales[weekday].totals.total}></input>
                                 </div>
                                 <div class=${styles['hourly-totals-field'] }>
                                     <label for="total-percentage">Weekly Share</label>
-                                    <input class=${styles['manual-totals']} disabled id="total-percentage-${weekday}" value=${hourlySales[weekday].totals.share}></input>
                                     <p>%</p>
+                                    <input class=${styles['manual-totals']} disabled id="total-percentage-${weekday}" value=${hourlySales[weekday].totals.share}></input>
                                 </div>
                             </div>
                         </div>
